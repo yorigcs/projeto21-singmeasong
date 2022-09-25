27c9b3ef0074d4c36227820020c7c0fec0faf93d
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-25 18:51:36 -0300

    test: ensure returns 409 if already has a recommendation with same name

--- back-end/tests/integration/recommendation.test.ts
+++ back-end/tests/integration/recommendation.test.ts
@@ -3,7 +3,7 @@ import app from '../../src/app'
 import supertest from 'supertest'
 import { prisma } from '../../src/database'
 import { recommendation } from '../factories/recommendations'
-
+import { recommendationService } from '../../src/services/recommendationsService'
 beforeEach(async () => {
   await prisma.$executeRaw`TRUNCATE TABLE recommendations;`
 })
@@ -22,13 +22,6 @@ describe('POST /recommendations', () => {
   it('should returns 422 if youtubeLink is not provided', async () => {
     const reccomendationData = recommendation()
     const { id, score, ...data } = reccomendationData
-    const request = await supertest(app).post('/recommendations').send({ name: data.name })
-    expect(request.status).toBe(422)
-  })
-
-  it('should returns 422 if youtubeLink is not a valid link', async () => {
-    const reccomendationData = recommendation()
-    const { id, score, ...data } = reccomendationData
     const request = await supertest(app).post('/recommendations').send(Object.assign({}, data, { youtubeLink: 'any_invalid_url' }))
     expect(request.status).toBe(422)
   })
@@ -38,5 +31,13 @@ describe('POST /recommendations', () => {
     const { id, score, ...data } = reccomendationData
     const request = await supertest(app).post('/recommendations').send(data)
     expect(request.status).toBe(201)
+  })
+
+  it('should returns 409 if already has a recommendation with same name', async () => {
+    const reccomendationData = recommendation()
+    const { id, score, ...data } = reccomendationData
+    await recommendationService.insert(data)
+    const request = await supertest(app).post('/recommendations').send(data)
+    expect(request.status).toBe(409)
   })
 })
