d21b9b51976d7d4888c5af91f6dad9e5664fcfb2
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-25 18:47:28 -0300

    fix: youtubeLink uri

--- back-end/tests/factories/recommendations.ts
+++ back-end/tests/factories/recommendations.ts
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker'
 export const recommendation = (): Recommendation => ({
   id: faker.datatype.number({ min: 1, max: 10 }),
   name: faker.random.words(4),
-  youtubeLink: faker.internet.url(),
+  youtubeLink: `https://youtu.be/${faker.random.alphaNumeric(5)}`,
   score: faker.datatype.number({ min: 1, max: 1000 })
 })
 
