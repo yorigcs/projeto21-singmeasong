972ce1ec4dd7b129495be1077e498179f124c285
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-25 18:39:42 -0300

    test: ensure returns 422 if name is not provided

---
+++ back-end/tests/integration/recommendation.test.ts
@@ -0,0 +1,21 @@
+
+import app from '../../src/app'
+import supertest from 'supertest'
+import { prisma } from '../../src/database'
+import { recommendation } from '../factories/recommendations'
+
+beforeEach(async () => {
+  await prisma.$executeRaw`TRUNCATE TABLE recommendations;`
+})
+afterAll(async () => {
+  await prisma.$disconnect()
+})
+
+describe('POST /recommendations', () => {
+  it('should returns 422 if name is not provided', async () => {
+    const reccomendationData = recommendation()
+    const { id, score, ...data } = reccomendationData
+    const request = await supertest(app).post('/recommendations').send({ youtubeLink: data.youtubeLink })
+    expect(request.status).toBe(422)
+  })
+})
